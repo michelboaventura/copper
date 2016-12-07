9fddd525405df3e333e2936ce2936d8ee5a5e97f
Matheus Gonçalves <souzagonc@gmail.com>
2016-12-07 15:12:51 -0200

    model database

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -1,18 +1,14 @@
 class DatabasesController < ApplicationController
 
   def index
-    biding.pry
     render json: Database.all.as_json
   end
   def create
-
     database = Database.new(params)
-    binding.pry
     database.save
     render json: database.as_json
   end
   def show
-    binding.pry
     render json: Database.find(params[:id])
   end
 
--- app/models/database.rb
+++ app/models/database.rb
@@ -1,3 +1,8 @@
 class Database
   include Mongoid::Document
+  field :name, type: String
+  field :user_id, type: Integer
+  field :description, type: String
+  field :data_type, type: String
+  field :size, type: String
 end
