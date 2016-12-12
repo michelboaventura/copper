96d82e8bdb133aef8d8cd265c803d6668a106536
Michel Boaventura <michel.boaventura@gmail.com>
2016-12-12 15:28:08 -0200

    Ruby version

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'ai_social_rails', user: 'ubuntu', roles: %w{app db web}
+server 'citron.ctweb.inweb.org.br', user: 'ubuntu', roles: %w{app db web}
 
 # role-based syntax
 # ==================
