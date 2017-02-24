f7d3f18f3a2f4166abcf15632ee4985ab826447b
Pedro Bernardina <pedro.nascimento@dcc.ufmg.br>
2017-02-24 15:48:28 -0300

    changed cards order

--- frontend/app/components/visualizations/vis-cards-container.js
+++ frontend/app/components/visualizations/vis-cards-container.js
@@ -4,25 +4,11 @@ export default Ember.Component.extend({
 
   visualizations: [
     {
-      name:   "Correlation Matrix",
-      icon:   "fa-list",
-      image:  "icon-th-outline",
-      vis:    "correlation-matrix",
-      tooltip: "Correlation Metrics comments Authors x Law Articles.",
-    },
-    {
-      name:   "Sentiment Analysis",
-      icon:   "fa-list",
-      image:  "icon-emo-thumbsup",
-      vis:    "sentiment-analysis",
-      tooltip: "Sentiment Analysis metrics of comments in law articles.",
-    },
-    {
-      name:   "Topicos",
-      icon:   "fa-bar-chart",
-      image:  "icon-newspaper",
-      vis:     "topicos-vis",
-      tooltip: "This visualization shows a table between topics and axis.",
+      name:   "Search Tool",
+      icon:   "fa-search",
+      image:  "icon-search",
+      vis:    "search-tool",
+      tooltip: "Exploratory Tool which can be used to search the filtered comments.",
     },
     {
       name:   "Word Tree",
@@ -30,13 +16,6 @@ export default Ember.Component.extend({
       image:  "icon-flow-split",
       vis:    "wordtree-diagram",
       tooltip: "Wordtree Analysis visualization of all of the filtered comments.",
-    },
-    {
-      name:   "Search Tool",
-      icon:   "fa-search",
-      image:  "icon-search",
-      vis:    "search-tool",
-      tooltip: "Exploratory Tool which can be used to search the filtered comments.",
     },
     {
       name:   "Co-ocurrence",
@@ -58,6 +37,27 @@ export default Ember.Component.extend({
       image:  "icon-users",
       vis:    "part-item",
       tooltip: "Graph showing relationship between participants and commentables.",
+    },
+    {
+      name:   "Sentiment Analysis",
+      icon:   "fa-list",
+      image:  "icon-emo-thumbsup",
+      vis:    "sentiment-analysis",
+      tooltip: "Sentiment Analysis metrics of comments in law articles.",
+    },
+    {
+      name:   "Correlation Matrix",
+      icon:   "fa-list",
+      image:  "icon-th-outline",
+      vis:    "correlation-matrix",
+      tooltip: "Correlation Metrics comments Authors x Law Articles.",
+    },
+    {
+      name:   "Topicos",
+      icon:   "fa-bar-chart",
+      image:  "icon-newspaper",
+      vis:     "topicos-vis",
+      tooltip: "This visualization shows a table between topics and axis.",
     },
   ],
 });
