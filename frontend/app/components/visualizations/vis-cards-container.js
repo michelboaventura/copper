b4cdfb82e3976464388bf9477f2c4e943b289c4e
Pedro Bernardina <pedro.nascimento@dcc.ufmg.br>
2017-02-24 14:10:03 -0300

    New Vis Cards for Job Results

---
+++ frontend/app/components/visualizations/vis-cards-container.js
@@ -0,0 +1,67 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  init() {
+    this._super(...arguments);
+    console.log(this.get("jobId"));
+  },
+
+  visualizations: [
+    {
+      name:   "Correlation Matrix",
+      icon:   "fa-list",
+      image:  "icon-th-outline",
+      vis:    "correlation-matrix",
+      tooltip: "Correlation Metrics comments Authors x Law Articles.",
+    },
+    {
+      name:   "Sentiment Analysis",
+      icon:   "fa-list",
+      image:  "icon-emo-thumbsup",
+      vis:    "sentiment-analysis",
+      tooltip: "Sentiment Analysis metrics of comments in law articles.",
+    },
+    {
+      name:   "Topicos",
+      icon:   "fa-bar-chart",
+      image:  "icon-newspaper",
+      vis:     "topicos-vis",
+      tooltip: "This visualization shows a table between topics and axis.",
+    },
+    {
+      name:   "Word Tree",
+      icon:   "icon-flow-tree",
+      image:  "icon-flow-split",
+      vis:    "visualizations.wordtree-diagram",
+      tooltip: "Wordtree Analysis visualization of all of the filtered comments.",
+    },
+    {
+      name:   "Search Tool",
+      icon:   "fa-search",
+      image:  "icon-search",
+      vis:    "search-tool",
+      tooltip: "Exploratory Tool which can be used to search the filtered comments.",
+    },
+    {
+      name:   "Co-ocurrence",
+      icon:   "icon-flow-tree",
+      image:  "icon-comment-1",
+      vis:    "graph-canvas",
+      tooltip: "Co-ocurrence visualization of words within filtered comments.",
+    },
+    {
+      name:   "Co-ocurrence - Full Data",
+      icon:   "icon-flow-tree",
+      image:  "icon-chat-2",
+      vis:    "graph-canvas-full",
+      tooltip: "Co-ocurrence visualization of words within filtered comments.",
+    },
+    {
+      name:   "Participants - Itens",
+      icon:   "icon-flow-tree",
+      image:  "icon-users",
+      vis:    "part-item",
+      tooltip: "Graph showing relationship between participants and commentables.",
+    },
+  ],
+});
