e666a6f53e556fbad82ac2a43205b922046a0824
Michel Boaventura <michel.boaventura@gmail.com>
2017-02-25 20:33:33 -0300

    Atualiza release date

--- frontend/app/routes/home/index.js
+++ frontend/app/routes/home/index.js
@@ -11,7 +11,7 @@ export default Ember.Route.extend({
       releaseNotes: {
         message: "This is the version 2.0 of application Lemonade! With new operations, faster and simple to use this version brings the power of Apache Spark to execute data minning as easy as build a flow." ,
         version: "2.0",
-        date: "01/01/2017"},
+        date: "15/02/2017"},
       videoTutorial: {
         title: "Basic Tutorial",
         youtubeID: "5_sHXJC9ocA"
