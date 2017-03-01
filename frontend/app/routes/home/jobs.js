bc2a1afd8ab8ab17196afb1e6248a39a8abacc03
Michel Boaventura <michel.boaventura@gmail.com>
2017-03-01 13:47:23 -0300

    Inverte ordem dos jobs e melhora botão de job vazio

--- frontend/app/routes/home/jobs.js
+++ frontend/app/routes/home/jobs.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   currentUser: Ember.inject.service('current-user'),
   model() {
-    return this.store.query('job', { user_id: this.get('currentUser').id });
+    return this.store
+      .query('job', { user_id: this.get('currentUser').id }).then((results) => {
+        return results.sortBy('created_at').reverse();
+      });
   },
 });
