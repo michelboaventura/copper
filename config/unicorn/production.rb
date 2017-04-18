08c3231be08517a7d55d42dc66f556e64ea05c81
Israel Guerra <israel.guerra@gmail.com>
2017-04-18 15:27:05 -0300

    Corrige paths no deploy de production

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,7 +1,7 @@
-app_path = "/home/ubuntu/ai_social_rails"
+app_path = "/home/ceweb/mj_data_explorer"
 working_directory "#{app_path}/current"
 worker_processes 4
-listen "/tmp/ai_social_rails.sock", backlog: 64
+listen "/tmp/mj_data_explorer.sock", backlog: 64
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 timeout 60
 stderr_path "log/unicorn.stderr.log"
