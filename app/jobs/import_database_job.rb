205bf21269bfe0fb38bc2cd8061d29a194c7e771
Michel Boaventura <michel.boaventura@gmail.com>
2017-04-18 11:31:25 -0300

    Trata problema de ids duplicados no json

--- app/jobs/import_database_job.rb
+++ app/jobs/import_database_job.rb
@@ -10,12 +10,18 @@ class ImportDatabaseJob < ActiveJob::Base
   def import(file, db:)
     author_names = Set.new
     json = JSON.parse(file)
+    hash = {}
+    parents = {}
 
     json['parts'].each do |part_json|
+      part_id   = part_json.delete('id')
+      parent_id = part_json.delete('parent_id')
       part = Part.new(part_json)
       part.database = db
       begin
         part.save!
+        hash[part_id] = part.id
+        parents[part_id] = parent_id if parent_id != nil
       rescue StandardError => e
         db.parts.delete_all
         db.delete
@@ -23,12 +29,20 @@ class ImportDatabaseJob < ActiveJob::Base
       end
     end
 
+    parents.each_pair do |old_child_id, old_parent_id|
+      new_child_id = hash[old_child_id]
+      new_parent_id = hash[old_parent_id]
+      Part.find(new_child_id).update_attribute(:parent_id, new_parent_id)
+    end
+
     json['comments'].each do |comment_json|
+      comment_json.delete('id')
       comment = Comment.new(comment_json)
       comment.database = db
       comment.author_id = Digest::MD5.hexdigest(comment.author_name)
       author_names << comment.author_name
       comment.author_id = author_names.find_index(comment.author_name)
+      comment.part_id = hash[comment_json['part_id']]
       begin
         comment.save!
       rescue StandardError => e
