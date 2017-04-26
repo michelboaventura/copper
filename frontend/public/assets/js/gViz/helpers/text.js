d2474eed45e94cc9bd867143b71d622c8afc12b1
Geraldo Ribeiro Franciscani <gfranciscani@gmail.com>
2017-04-26 17:37:24 -0300

    Fix pull request

--- frontend/public/assets/js/gViz/helpers/text.js
+++ frontend/public/assets/js/gViz/helpers/text.js
@@ -13,7 +13,6 @@ gViz.helpers.text.getBBox = function (container) {
 
   var o = container.append("text").style("font-weight", weight).style("font-size", fontSize + "px").style("visibility", "hidden !important").text(text);
   var bbox = o.node().getBBox();
-  var bbox = o.node().getBBox();
   o.remove();
   return bbox;
 };
