d559b2cacf1443fdd27362efe3598daabe24589a
Michel Boaventura <michel.boaventura@gmail.com>
2017-04-26 13:01:34 -0300

    Passa JSON pelo rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :log_level, :info
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/json', 'frontend/node_modules', 'frontend/bower_components')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'data', 'public/json', 'frontend/node_modules', 'frontend/bower_components')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :datasources, controller: 'databases'
   resources :jobs, controller: 'jobs'
 
+  get '/json/:job_id/:visualization', to: 'json#index'
+
   Rails.application.routes.draw do
     mount_ember_app :frontend, to: "/"
 
