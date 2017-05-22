c123a3be0cd03177d7ec1b9f48166a6c09a65343
Matheus Gonçalves <souzagonc@gmail.com>
2017-05-22 16:25:41 -0300

    migração do login

---
+++ frontend/app/controllers/landing-page/login.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+const { service } = Ember.inject;
+
+export default Ember.Controller.extend({
+  session: service(),
+  sessionAccount: service(),
+
+  actions: {
+    authenticate(){
+      this.set('emailErrorMessage', null);
+      this.set('passwordErrorMessage', null);
+
+      let { email, password } = this.getProperties('email','password');
+
+      this.get('session').authenticate('authenticator:oauth2', email, password).then(() => {
+          this.get('sessionAccount').loadCurrentUser();
+        }).catch((reason)=>{
+          if(reason.errors){
+            this.set('emailErrorMessage', reason.errors[0].detail || reason);
+            this.set('passwordErrorMessage', reason.errors[1].detail || reason);
+          }
+        });
+    },
+  },
+});
+
