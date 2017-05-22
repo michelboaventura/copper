951ef5603f6425c8e59b66f8e0938b9bf537ab92
Matheus Gonçalves <souzagonc@gmail.com>
2017-05-22 18:24:11 -0300

    Migração da navbar lateral
    Migração da navbar lateral e rotas necessarias

---
+++ frontend/tests/unit/routes/ferramenta/filtros/adicionar-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:ferramenta/filtros/adicionar', 'Unit | Route | ferramenta/filtros/adicionar', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
