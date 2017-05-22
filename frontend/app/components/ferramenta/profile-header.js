1db9d244175edbeba3897c0711f459a873026ebb
Matheus Gonçalves <souzagonc@gmail.com>
2017-05-22 18:44:29 -0300

    dropdown do profile header

--- frontend/app/components/ferramenta/profile-header.js
+++ frontend/app/components/ferramenta/profile-header.js
@@ -7,6 +7,19 @@ export default Ember.Component.extend({
   session: service(),
   serverTokenEndpoint: `${config.mj_data_explorer}/users/sign_out`,
 
+  didInsertElement(){
+    var linkListContainer = document.querySelector('.link-list-wrapper');
+    var linkListActionButton = document.querySelector('.link-list__action');
+
+    linkListActionButton.addEventListener('click', function () {
+      if (linkListContainer.classList.contains('is-open')) {
+        linkListContainer.classList.remove('is-open');
+      } else {
+        linkListContainer.classList.add('is-open');
+      }
+    });
+  },
+
   actions: {
     invalidateSession(){
       var serverEndpoint = this.get('serverTokenEndpoint');
