ba181b0320aefff339c522210c91b727fbb4ebff
Michel Boaventura <michel.boaventura@gmail.com>
2017-06-01 11:19:03 -0300

    Correções estéticas

--- frontend/app/routes/landing-page/filtros/index.js
+++ frontend/app/routes/landing-page/filtros/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-     return this.store.query('job', { public: true })
-    },
+    return this.store.query('job', { public: true })
+  },
 
 });
