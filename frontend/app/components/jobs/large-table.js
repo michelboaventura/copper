8656635b68f7bbf1cbd2c83cd9c953f5206fac3a
Matheus Gonçalves <souzagonc@gmail.com>
2017-06-02 16:08:39 -0300

    remove arquivos do citron não utilizados

--- frontend/app/components/jobs/large-table.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  sortedModel: Ember.computed.sort('model', 'sortDefinition'),
-  sortDefinition: ['created_at:desc'],
-});
--- frontend/app/router.js
+++ frontend/app/router.js
@@ -7,44 +7,33 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('datasource', function() {
-    this.route('new');
-    this.route('edit', {path: ':id/edit'});
-  });
-
   this.route('landing-page', function() {
     this.route('contato');
     this.route('cadastro');
     this.route('sobre');
     this.route('login');
-
     this.route('filtros', function() {
       this.route('aberto',{path: ':id/aberto'});
       this.route('resultado', {path: ':id/resultado/:which'}, function() {});
     });
-
     this.route('bases', function() {
       this.route('aberta',{path: ':id/aberta'});
     });
   });
 
   this.route('ferramenta', function() {
-
     this.route('perfil', function() {
       this.route('senha');
     });
-
     this.route('bases', function() {
       this.route('adicionar');
       this.route('editar', {path: ':id/editar'});
     });
-
     this.route('filtros', function() {
       this.route('adicionar');
       this.route('resultado', {path: ':id/resultado/:which'});
       this.route('aberto', {path: ':id/aberto'});
     });
-
     this.route('sobre');
   });
 });
