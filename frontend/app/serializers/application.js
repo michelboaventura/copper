73edf5421279c4614932edfa91b149b2d77f3b29
Michel Boaventura <michel.boaventura@gmail.com>
2017-06-02 21:56:05 -0300

    Corrige alguns warnings

--- frontend/app/serializers/application.js
+++ frontend/app/serializers/application.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
   normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
-    let normalizedDocument = this._super(...arguments);
-    normalizedDocument.meta = {total_pages: normalizedDocument.meta['total-pages']}
+    let normalizedDocument = this._super(store, primaryModelClass, payload, id, requestType);
+    normalizedDocument.meta = {total_pages: normalizedDocument.meta['total-pages']};
     return normalizedDocument;
   },
 });
