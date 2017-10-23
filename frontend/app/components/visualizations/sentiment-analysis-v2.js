d32238d8d15526a888f2079664a66b4ae2ce4c5d
Pedro Bernardina <pedronascimento@dcc.ufmg.br>
2017-10-23 13:38:32 -0200

    Finaliza cabeçalho da nova vis

---
+++ frontend/app/components/visualizations/sentiment-analysis-v2.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  init() {
+    this._super(...arguments);
+  },
+
+  // Attributes bingins
+  width:  function(){ return this.get('width'); }.property('width'),
+  height: function(){ return this.get('height'); }.property('height'),
+  _id:    function(){ return this.get('_id'); }.property('_id'),
+  style:  function(){ return "width:"+this.get('width')+"; height:"+this.get('height')+";"; }.property('style'),
+
+  // Chart var
+  _var: null,
+
+  didInsertElement: function(){
+    let self = this;
+    let title = "Reforma do Ensino Médio";
+    let description = "Medida Provisória n 746 de 2016."
+    let articles = "197";
+    let participations = "21.763";
+
+    self.set("title", title);
+    self.set("description", description);
+    self.set("articles", articles);
+    self.set("participations", participations);
+
+  }
+});
--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -36,7 +36,8 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.mj_data_explorer = 'http://localhost:8000'
+    // ENV.mj_data_explorer = 'http://localhost:8000'
+    ENV.mj_data_explorer = 'http://staging-ceweb.speed.dcc.ufmg.br';
   }
 
   if (environment === 'test') {
