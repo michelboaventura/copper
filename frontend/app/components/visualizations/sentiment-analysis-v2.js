42b750a964106b522f76cace8d41189599a6d82f
Pedro Bernardina <pedronascimento@dcc.ufmg.br>
2017-10-30 15:45:34 -0200

    Adiciona expansão para o texto dos artigos

--- frontend/app/components/visualizations/sentiment-analysis-v2.js
+++ frontend/app/components/visualizations/sentiment-analysis-v2.js
@@ -16,21 +16,53 @@ export default Ember.Component.extend({
 
   didInsertElement: function(){
     let self = this;
-    let title = "Reforma do Ensino Médio";
-    let description = "Medida Provisória n 746 de 2016."
-    let articles = "197";
-    let participations = "21.763";
-    let min_participations = "150";
 
-    self.set("title", title);
-    self.set("description", description);
-    self.set("articles", articles);
-    self.set("participations", participations);
-    self.set("min_participations", min_participations);
-
+    // Activate icon when clicked
     $(".emoticon").on("click", function() {
       if($(this).hasClass("active")) { $(this).removeClass("active") }
       else { $(this).addClass("active") }
     });
+
+    $(".expand-article").on("click", function() {
+      if($(this).hasClass("active")) {
+        $(".article-text").addClass("has-max-height");
+        $(this).removeClass("active")
+      }
+      else {
+        $(".article-text").removeClass("has-max-height");
+        $(this).addClass("active")
+      }
+    });
+
+    $.ajax({
+      url: "/assets/data/sentiment-analysis-v2.json",
+      type: "GET",
+      beforeSend() { gViz.helpers.loading.show(); },
+      success(response) {
+        self.set("title", response["title"]);
+        self.set("description", response["description"]);
+        self.set("participations-total", response["participations-total"]);
+        self.set("participations-min", response["participations-minmax"][0]);
+        self.set("participations-max", response["participations-minmax"][1]);
+        self.set("articles-total", response["articles-total"]);
+        self.set("articles", response["articles"]);
+      },
+      error() { gViz.helpers.loading.hide(); },
+      complete() {  gViz.helpers.loading.hide(); }
+    });
+  },
+
+  /*
+  didRender() {
+    let self = this;
+    let i = 0;
+
+    let articles_texts = document.getElementsByClassName("article-text");
+
+    for(i = 0; i < articles_texts.length; i++) {
+      let article = articles_texts[i];
+      $clamp(article, { clamp: 'auto' });
+    }
   }
+  */
 });
